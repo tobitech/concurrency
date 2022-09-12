3e9c4d282151b52574cef3f2ba46db22df9a0394
Oluwatobi Omotayo <tobitech@ymail.com>
2022-09-12 20:32:01 +0100

    explore what it means to run something on a main application thread and a different thread with detachNewThread closure.

--- Sources/concurrency/main.swift
+++ Sources/concurrency/main.swift
@@ -1,1 +1,12 @@
+import Foundation
 
+Thread.detachNewThread {
+  // do some work
+  Thread.sleep(forTimeInterval: 1)
+  print(Thread.current)
+}
+
+// calling `Thread.sleep` is on the main thread of the executable application.
+// call it inside the detachNewThread closure is sleeping on another thread different from the main thread of the application.
+print(Thread.current)
+Thread.sleep(forTimeInterval: 1.1)
