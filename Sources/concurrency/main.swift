5ec40e0f10e4ccefd1e85a2d311cfc27e8113eed
Oluwatobi Omotayo <tobitech@ymail.com>
2022-09-12 20:33:15 +0100

    explore what it is to start multiple threads with detachNewThread

--- Sources/concurrency/main.swift
+++ Sources/concurrency/main.swift
@@ -1,12 +1,30 @@
 import Foundation
 
+// It turns out threads are not started in the same way they were created.
 Thread.detachNewThread {
   // do some work
-  Thread.sleep(forTimeInterval: 1)
-  print(Thread.current)
+  // Thread.sleep(forTimeInterval: 1)
+  // print(Thread.current)
+  print("1", Thread.current)
+}
+
+Thread.detachNewThread {
+  print("2", Thread.current)
+}
+
+Thread.detachNewThread {
+  print("3", Thread.current)
+}
+
+Thread.detachNewThread {
+  print("4", Thread.current)
+}
+
+Thread.detachNewThread {
+  print("5", Thread.current)
 }
 
 // calling `Thread.sleep` is on the main thread of the executable application.
 // call it inside the detachNewThread closure is sleeping on another thread different from the main thread of the application.
-print(Thread.current)
+// print(Thread.current)
 Thread.sleep(forTimeInterval: 1.1)
