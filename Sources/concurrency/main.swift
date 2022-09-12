e2fb6beae34a1d57ef953487113d59af93d41b2b
Oluwatobi Omotayo <tobitech@ymail.com>
2022-09-12 20:56:29 +0100

    explore how we can hold on to a thread, inspect it, call methods on it and more.
    also explore the concept of priority and cooperative cancellation

--- Sources/concurrency/main.swift
+++ Sources/concurrency/main.swift
@@ -1,30 +1,62 @@
 import Foundation
 
 // It turns out threads are not started in the same way they were created.
-Thread.detachNewThread {
-  // do some work
-  // Thread.sleep(forTimeInterval: 1)
-  // print(Thread.current)
-  print("1", Thread.current)
+
+func threadBasics() {
+  Thread.detachNewThread {
+    print("1", Thread.current)
+  }
+  
+  Thread.detachNewThread {
+    print("2", Thread.current)
+  }
+  
+  Thread.detachNewThread {
+    print("3", Thread.current)
+  }
+  
+  Thread.detachNewThread {
+    print("4", Thread.current)
+  }
+  
+  Thread.detachNewThread {
+    print("5", Thread.current)
+  }
 }
 
-Thread.detachNewThread {
-  print("2", Thread.current)
+let thread = Thread {
+  // we work we do in this closure is lazy.
+  
+  // to demonstrate how long the Thread slept for
+  let start = Date()
+  defer { print("Finished in", Date().timeIntervalSince(start)) }
+  
+  // this doesn't perticipate in cooperative cancellation, so it will wait it full time even though it's cancelled.
+  Thread.sleep(forTimeInterval: 1)
+  
+  // this is how co-operative cancellation works.
+  
+  guard !Thread.current.isCancelled else {
+    print("Cancelled!")
+    return
+  }
+  
+  print(Thread.current) // print out what thread we're on.
 }
 
-Thread.detachNewThread {
-  print("3", Thread.current)
-}
+// this allows us to set thread priority: a Double value between 0 and 1
+// This will signal to the operating system that this thread is low or high priority, and perhaps the OS will give the thread a little bit less or more time to execute relative to other threads, though there are no guarantees.
+thread.threadPriority = 0.75
 
-Thread.detachNewThread {
-  print("4", Thread.current)
-}
+// we need to explicitly start the thread since the work is lazy.
+// otherwise the program exits immediately.
+thread.start()
 
-Thread.detachNewThread {
-  print("5", Thread.current)
-}
+// this should give the thread just enough time to start execution
+// otherwise the os just doesn't bother to start the thread since it was cancelled immediately.
+Thread.sleep(forTimeInterval: 0.01)
 
-// calling `Thread.sleep` is on the main thread of the executable application.
-// call it inside the detachNewThread closure is sleeping on another thread different from the main thread of the application.
-// print(Thread.current)
+// this allows us to cancel a thread
+thread.cancel()
+
 Thread.sleep(forTimeInterval: 1.1)
