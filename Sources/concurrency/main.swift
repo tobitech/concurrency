db47ff3d390974ad536e030668771c8f6bc7803d
Oluwatobi Omotayo <tobitech@ymail.com>
2022-09-13 12:05:49 +0100

    explore how dispatch queue can still accidentally explode the number of threads in use if we're not careful.
    in this case we're creating a queue for a single unit of work.

--- Sources/concurrency/main.swift
+++ Sources/concurrency/main.swift
@@ -78,90 +78,102 @@ func dispatchPriorityAndCancellation() {
   item.cancel()
 }
 
-func makeDatabaseQuery() {
-  let requestId = DispatchQueue.getSpecific(key: requestIdKey)!
-  print(requestId, "Making database query")
-  Thread.sleep(forTimeInterval: 0.5)  // simulate the idea of making a database query by sleeping for 0.5sec
-  print(requestId, "Finished database query")
-}
-
-func makeNetworkRequest() {
-  let requestId = DispatchQueue.getSpecific(key: requestIdKey)!
-  print(requestId, "Making network request")
-  Thread.sleep(forTimeInterval: 0.5)
-  print(requestId, "Finished network request")
-}
-
-// we are passing along the queue so that we can use it to target the other child queues and have access to the `specifics`.
-func response(for request: URLRequest, queue: DispatchQueue) -> HTTPURLResponse {
-  // TODO: do the work to turn request into a response
-  // somehow it finds a way to know what queue we're operating on and get the specific key.
-  let requestId = DispatchQueue.getSpecific(key: requestIdKey)!
-  
-  let start = Date()
-  defer { print("Finished in", Date().timeIntervalSince(start)) }
-  
-  // this is used for coordination. it allows us to treat multiple unit of work as just a single unit of work.
-  // if we don't use this, because the databaseQueue and networkQueue are non-blocking it will just breeze past them and return an empty HTTPURLResponse.
-  // so we need a way to wait for the two works to be completed.
-  let group = DispatchGroup()
-  
-  // so now we can do some units of work, to generate the response e.g. database query, network request, wrap each of them with a log, so we know what is happening on the inside of this request lifecycle.
-  let databaseQueue = DispatchQueue(label: "database-query", target: queue)
-  // invoking async on this queue is a non-blocking operation
-  databaseQueue.async(group: group) {
-    makeDatabaseQuery()
+func dispatchStorageAndCoordination() {
+  func makeDatabaseQuery() {
+    let requestId = DispatchQueue.getSpecific(key: requestIdKey)!
+    print(requestId, "Making database query")
+    Thread.sleep(forTimeInterval: 0.5)  // simulate the idea of making a database query by sleeping for 0.5sec
+    print(requestId, "Finished database query")
   }
   
-  let networkQueue = DispatchQueue(label: "network-request", target: queue)
-  networkQueue.async(group: group) {
-    makeNetworkRequest()
+  func makeNetworkRequest() {
+    let requestId = DispatchQueue.getSpecific(key: requestIdKey)!
+    print(requestId, "Making network request")
+    Thread.sleep(forTimeInterval: 0.5)
+    print(requestId, "Finished network request")
   }
   
-  // remember for threads we had to do some polling to check if the two operations had finished before we could return a response.
-  // DispatchGroup is a nice tool for coordination here.
-  group.wait()
+  // we are passing along the queue so that we can use it to target the other child queues and have access to the `specifics`.
+  func response(for request: URLRequest, queue: DispatchQueue) -> HTTPURLResponse {
+    // TODO: do the work to turn request into a response
+    // somehow it finds a way to know what queue we're operating on and get the specific key.
+    let requestId = DispatchQueue.getSpecific(key: requestIdKey)!
+    
+    let start = Date()
+    defer { print("Finished in", Date().timeIntervalSince(start)) }
+    
+    // this is used for coordination. it allows us to treat multiple unit of work as just a single unit of work.
+    // if we don't use this, because the databaseQueue and networkQueue are non-blocking it will just breeze past them and return an empty HTTPURLResponse.
+    // so we need a way to wait for the two works to be completed.
+    let group = DispatchGroup()
+    
+    // so now we can do some units of work, to generate the response e.g. database query, network request, wrap each of them with a log, so we know what is happening on the inside of this request lifecycle.
+    let databaseQueue = DispatchQueue(label: "database-query", target: queue)
+    // invoking async on this queue is a non-blocking operation
+    databaseQueue.async(group: group) {
+      makeDatabaseQuery()
+    }
+    
+    let networkQueue = DispatchQueue(label: "network-request", target: queue)
+    networkQueue.async(group: group) {
+      makeNetworkRequest()
+    }
+    
+    // remember for threads we had to do some polling to check if the two operations had finished before we could return a response.
+    // DispatchGroup is a nice tool for coordination here.
+    group.wait()
+    
+    // TODO: return real response
+    return .init()
+  }
   
-  // TODO: return real response
-  return .init()
+  // we can create a single queue, when the server first loads up.
+  let serverQueue = DispatchQueue(label: "server-queue", attributes: .concurrent)
+  
+  let requestIdKey = DispatchSpecificKey<UUID>()
+  let requestId = UUID()
+  // remember to set concurrent attribute to make it the work sent to it happen in parallel
+  let requestQueue = DispatchQueue(label: "request-\(requestId)", attributes: .concurrent, target: serverQueue)
+  
+  // This allows us to pluck the request ID out of thin air without having to explicitly pass it through every single layer:
+  // as long as we are operating within the execution context of this `queue`, we will have access to it.
+  requestQueue.setSpecific(key: requestIdKey, value: requestId)
+  
+  let item = DispatchWorkItem {
+    response(for: .init(url: .init(string: "http://pointfree.co")!), queue: requestQueue)
+  }
+  
+  requestQueue.async(execute: item)
+  
+  let queue1 = DispatchQueue(label: "queue1")
+  let idKey = DispatchSpecificKey<Int>()
+  let dateKey = DispatchSpecificKey<Date>()
+  queue1.setSpecific(key: idKey, value: 42)
+  queue1.setSpecific(key: dateKey, value: Date())
+  
+  queue1.async {
+    print("queue1", "id", DispatchQueue.getSpecific(key: idKey))
+    print("queue1", "date", DispatchQueue.getSpecific(key: dateKey))
+    
+    // let queue2 = DispatchQueue(label: "queue2")
+    // setting a target is how it would inherit the specifics of the queue it's targetting.
+    let queue2 = DispatchQueue(label: "queue2", target: queue1)
+    queue2.setSpecific(key: idKey, value: 1729)
+    queue2.async {
+      print("queue2", "id", DispatchQueue.getSpecific(key: idKey))
+      // specifics are not automatically inherited when you start a new queue inside the execution context of another queue.
+      print("queue2", "date", DispatchQueue.getSpecific(key: dateKey)) // nil
+    }
+  }
 }
 
-// we can create a single queue, when the server first loads up.
-let serverQueue = DispatchQueue(label: "server-queue", attributes: .concurrent)
-
-let requestIdKey = DispatchSpecificKey<UUID>()
-let requestId = UUID()
-// remember to set concurrent attribute to make it the work sent to it happen in parallel
-let requestQueue = DispatchQueue(label: "request-\(requestId)", attributes: .concurrent, target: serverQueue)
-
-// This allows us to pluck the request ID out of thin air without having to explicitly pass it through every single layer:
-// as long as we are operating within the execution context of this `queue`, we will have access to it.
-requestQueue.setSpecific(key: requestIdKey, value: requestId)
-
-let item = DispatchWorkItem {
-  response(for: .init(url: .init(string: "http://pointfree.co")!), queue: requestQueue)
-}
-
-requestQueue.async(execute: item)
-
-let queue1 = DispatchQueue(label: "queue1")
-let idKey = DispatchSpecificKey<Int>()
-let dateKey = DispatchSpecificKey<Date>()
-queue1.setSpecific(key: idKey, value: 42)
-queue1.setSpecific(key: dateKey, value: Date())
-
-queue1.async {
-  print("queue1", "id", DispatchQueue.getSpecific(key: idKey))
-  print("queue1", "date", DispatchQueue.getSpecific(key: dateKey))
-  
-  // let queue2 = DispatchQueue(label: "queue2")
-  // setting a target is how it would inherit the specifics of the queue it's targetting.
-  let queue2 = DispatchQueue(label: "queue2", target: queue1)
-  queue2.setSpecific(key: idKey, value: 1729)
-  queue2.async {
-    print("queue2", "id", DispatchQueue.getSpecific(key: idKey))
-    // specifics are not automatically inherited when you start a new queue inside the execution context of another queue.
-    print("queue2", "date", DispatchQueue.getSpecific(key: dateKey)) // nil
+// this creates about 500 threads, not a thousand so definitely DispatchQueue is doing some work in the background to limit the number of threads but the number is till pretty high.
+// so thread explotion is still possible.
+for n in 0..<workcount {
+  // we're creating a queue to run a single unit of work.
+  DispatchQueue(label: "queue-\(n)").async {
+    print(Thread.current)
+    while true {}
   }
 }
 
