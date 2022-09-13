80173ca47e41f5f3d0cd841d71b574bddaebe9f0
Oluwatobi Omotayo <tobitech@ymail.com>
2022-09-13 08:20:42 +0100

    explore the concept of dependencies with operation queues which allow you to start one operation after another finishes.

--- Sources/concurrency/main.swift
+++ Sources/concurrency/main.swift
@@ -16,42 +16,80 @@ func operationQueueBasics() {
   operation.addOperation { print("5", Thread.current) }
 }
 
-// let's eplore setting priorities on an operation.
-// to do that we have to hold on to the operation before handing it off to an operation queue.
-// to do that we have to create an instance of the operation class which means it needs to be subclassed, but Apple ships some subclasses we can use.
-
-// now we have access to the operation and we can use it inside the block
-let operation = BlockOperation()
-
-// this creates a retain-cycle because the closure is hold by the operation, and the closure is also referencing the operation inside of the block, so we need to weakify it or use unowned.
-// this is how to do cooperative cancellation.
-operation.addExecutionBlock { [unowned operation] in
+func operationPriorityAndCancellation() {
+  // let's eplore setting priorities on an operation.
+  // to do that we have to hold on to the operation before handing it off to an operation queue.
+  // to do that we have to create an instance of the operation class which means it needs to be subclassed, but Apple ships some subclasses we can use.
   
-  // running this to demonstrate that program ran for 1.0sec even though we cancelled it after 0.1sec
-  let start = Date()
-  defer { print("Finished", Date().timeIntervalSince(start)) }
+  // now we have access to the operation and we can use it inside the block
+  let operation = BlockOperation()
   
-  // It’s worth pointing that although cancellation is cooperative, the cooperation is not deeply ingrained in the system. Here we cancelled the task after 0.1 seconds, but the thread is still going to sleep for the full 1 second before continuing. There is no way to interrupt that sleeping:
-  Thread.sleep(forTimeInterval: 1)
-  guard !operation.isCancelled else {
-    print("Cancelled!")
-    return // short-circuit any other work.
+  // this creates a retain-cycle because the closure is hold by the operation, and the closure is also referencing the operation inside of the block, so we need to weakify it or use unowned.
+  // this is how to do cooperative cancellation.
+  operation.addExecutionBlock { [unowned operation] in
+    
+    // running this to demonstrate that program ran for 1.0sec even though we cancelled it after 0.1sec
+    let start = Date()
+    defer { print("Finished", Date().timeIntervalSince(start)) }
+    
+    // It’s worth pointing that although cancellation is cooperative, the cooperation is not deeply ingrained in the system. Here we cancelled the task after 0.1 seconds, but the thread is still going to sleep for the full 1 second before continuing. There is no way to interrupt that sleeping:
+    Thread.sleep(forTimeInterval: 1)
+    guard !operation.isCancelled else {
+      print("Cancelled!")
+      return // short-circuit any other work.
+    }
+    print(Thread.current)
   }
-  print(Thread.current)
+  
+  let queue = OperationQueue()
+  
+  // they call their own `qualityOfService` rather than priority which is a value from 0 and 1.
+  operation.qualityOfService = .background
+  
+  queue.addOperation(operation)
+  
+  Thread.sleep(forTimeInterval: 0.1) // to make sure it officially started.
+  // cancel the operation.
+  // we still get the print statements.
+  // like Thread, operation is cancellation is cooperative.
+  // it is up to us as good citizens to be checking if the item has been cancelled, so that we can short-circuit the remaining work that is left to be done.
+  operation.cancel()
 }
 
 let queue = OperationQueue()
 
-// they call their own `qualityOfService` rather than priority which is a value from 0 and 1.
-operation.qualityOfService = .background
+// create an operation that will run on the queue above.
+let operationA = BlockOperation {
+  print("A")
+  Thread.sleep(forTimeInterval: 1)
+}
 
-queue.addOperation(operation)
+let operationB = BlockOperation {
+  print("B")
+}
 
-Thread.sleep(forTimeInterval: 0.1) // to make sure it officially started.
-// cancel the operation.
-// we still get the print statements.
-// like Thread, operation is cancellation is cooperative.
-// it is up to us as good citizens to be checking if the item has been cancelled, so that we can short-circuit the remaining work that is left to be done.
-operation.cancel()
+let operationC = BlockOperation {
+  print("C")
+}
+
+let operationD = BlockOperation {
+  print("D")
+}
+
+// make operationB be dependent on operationA.
+// this means operationB will not be started until operationA finishes
+operationB.addDependency(operationA)
+operationC.addDependency(operationA)
+operationD.addDependency(operationB)
+operationD.addDependency(operationC)
+
+queue.addOperation(operationA)
+queue.addOperation(operationB)
+queue.addOperation(operationC)
+queue.addOperation(operationD)
+
+//A ➡️ B
+//⬇️    ⬇️
+//C ➡️ D
 
 Thread.sleep(forTimeInterval: 2)
