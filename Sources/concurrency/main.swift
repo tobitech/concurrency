0ed805b429c01d2d7c5b02f4cff3641c672e9cec
Oluwatobi Omotayo <tobitech@ymail.com>
2022-09-13 10:33:43 +0100

    explore how dispatch queue manages thread explosion.

--- Sources/concurrency/main.swift
+++ Sources/concurrency/main.swift
@@ -13,11 +13,11 @@ queue.async {
   print(Thread.current)
 }
 
-queue.async { print("1", Thread.current) }
-queue.async { print("2", Thread.current) }
-queue.async { print("3", Thread.current) }
-queue.async { print("4", Thread.current) }
-queue.async { print("5", Thread.current) }
+//queue.async { print("1", Thread.current) }
+//queue.async { print("2", Thread.current) }
+//queue.async { print("3", Thread.current) }
+//queue.async { print("4", Thread.current) }
+//queue.async { print("5", Thread.current) }
 
 // the units of work are run sequentially in order
 // and also all on the same thread.
@@ -28,5 +28,11 @@ queue.async { print("5", Thread.current) }
 //3 <NSThread: 0x10110c4a0>{number = 2, name = (null)}
 //4 <NSThread: 0x10110c4a0>{number = 2, name = (null)}
 //5 <NSThread: 0x10110c4a0>{number = 2, name = (null)}
+
+// just like operation queues, dispatch queues fixes the problem of thread explosion.
+// just few threads about 60 needed to run 1,000 units of work.
+for n in 0..<workcount {
+  queue.async { print(n, Thread.current) }
+}
 
 Thread.sleep(forTimeInterval: 2)
